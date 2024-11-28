f4fb3e7e66cfc5c4f907074ad7ea1d997c1827be
Szymon Cieśla <cszymon01@gmail.com>
2024-11-28 01:47:31 +0100

    created basic structure for main plugin file

---
+++ feedback-form.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Feedback form plugin
+ * Description: A plugin to submit feedback via a Gutenberg blocks and display it to users with admin privilages.
+ * Version: 1.0
+ * Author: Szymon Cieśla
+ * Text Domain: feedback form plugin
+ * Domain Path: /languages
+ */
+
+ defined('ABSPATH') || exit;
+
+require_once plugin_dir_path(__FILE__) . 'includes/class-form-plugin.php';
+
+use CustomFeedback\FeedbackPlugin;
+
+FeedbackPlugin::init();
